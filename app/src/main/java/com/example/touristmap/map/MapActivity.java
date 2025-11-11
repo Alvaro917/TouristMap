3c817887d68a892ecb5e4fecf25778d62a4166fc
AlvaroCM123 <a.cardenas56@alumnos.santotomas.cl>
2025-11-11 19:12:26 -0300

    se modifico el map acticity y se agregaron todos los puntos de ubicacion en un archivo json

--- app/src/main/java/com/example/touristmap/map/MapActivity.java
+++ app/src/main/java/com/example/touristmap/map/MapActivity.java
@@ -7,15 +7,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
-
 import com.example.touristmap.R;
-
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
@@ -23,35 +20,19 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity {
     private MapView mapView;
     private MyLocationNewOverlay locationOverlay;
-
-    // Inicialización de la lista de lugares usando Arrays.asList()
-    private final List<Lugar> todosLosLugares = Arrays.asList(
-            new Lugar("Cerro San Cristóbal", -33.4246, -70.6346, R.drawable.ic_park, "Parque"),
-            new Lugar("Parque Bicentenario", -33.40056, -70.60222, R.drawable.ic_park, "Parque"),
-            new Lugar("Parque Forestal", -33.4349, -70.6416, R.drawable.ic_park, "Parque"),
-            new Lugar("Museo de Bellas Artes", -33.4357, -70.6397, R.drawable.ic_museum, "Museo"),
-            new Lugar("Museo Histórico Nacional", -33.4374, -70.6511, R.drawable.ic_museum, "Museo"),
-            new Lugar("La Chascona (Casa Neruda)", -33.43116, -70.63448, R.drawable.ic_museum, "Cultura"),
-            new Lugar("Centro Cultural GAM", -33.4395, -70.6398, R.drawable.ic_museum, "Cultura"),
-            new Lugar("Costanera Center", -33.4170, -70.6068, R.drawable.ic_mall, "Tienda"),
-            new Lugar("Patio Bellavista", -33.43417, -70.63506, R.drawable.ic_mall, "Tienda"),
-            new Lugar("Estadio Nacional", -33.4631, -70.6106, R.drawable.ic_stadium, "Deporte"),
-            new Lugar("Estadio Monumental David Arellano", -33.50648, -70.60598, R.drawable.ic_stadium, "Deporte"),
-            new Lugar("Estadio Bicentenario de La Florida", -33.5378, -70.5737, R.drawable.ic_stadium, "Deporte"),
-            new Lugar("Estadio Santa Laura", -33.4027, -70.6554, R.drawable.ic_stadium, "Deporte"),
-            new Lugar("Estadio San Carlos de Apoquindo", -33.3909, -70.5004, R.drawable.ic_stadium, "Deporte"),
-            new Lugar("Fantasilandia", -33.4603, -70.6628, R.drawable.ic_atraccions, "Entretención"),
-            new Lugar("Templo Bahá'í", -33.47222, -70.50917, R.drawable.ic_tourism, "Turismo"),
-            new Lugar("Palacio de La Moneda", -33.4430, -70.6533, R.drawable.ic_tourism, "Turismo")
-    );
+    private List<Lugar> todosLosLugares = new ArrayList<>();
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
@@ -63,20 +44,20 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         setContentView(R.layout.activity_map);
+        todosLosLugares = lugaresJSON();
 
         mapView = findViewById(R.id.map);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setMultiTouchControls(true);
         mapView.getController().setZoom(14.0);
 
+        List<String> categorias = new ArrayList<>(Arrays.asList("Parque", "Tienda", "Museo", "Deporte", "Entretención", "Turismo"));
+        categorias.add(0, "Todos");
 
-        List<String> categorias = Arrays.asList("Parque", "Tienda", "Museo", "Deporte", "Entretención", "Turismo");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_custom, categorias);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         Spinner spinner = findViewById(R.id.filtromapa);
         spinner.setAdapter(adapter);
-
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -84,16 +65,12 @@ public class MapActivity extends AppCompatActivity {
                 String categoriaSeleccionada = parent.getItemAtPosition(position).toString();
                 filtrarMarcadores(categoriaSeleccionada);
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // No hacer nada
             }
         });
 
-        filtrarMarcadores("Todos");
-
-        // Manejo de permisos (convertido de 'when' a 'if-else')
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             activarCapadeUbicacion();
         } else {
@@ -143,6 +120,65 @@ public class MapActivity extends AppCompatActivity {
             mapView.getOverlays().add(marker);
         }
         mapView.invalidate();
+    }
+    private List<Lugar> lugaresJSON(){
+        List<Lugar> lugares = new ArrayList<>();
+        try {
+            String jsonString = loadJSONFromAsset();
+            if (jsonString == null){
+                return lugares;
+            }
+            JSONArray jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject lugarJson = jsonArray.getJSONObject(i);
+
+                String nombre = lugarJson.getString("nombre");
+                double latitud = lugarJson.getDouble("latitud");
+                double longitud = lugarJson.getDouble("longitud");
+                String iconoNombre = lugarJson.getString("icono");
+                String categoria = lugarJson.getString("categoria");
+
+                int iconoId = getIconoId(iconoNombre);
+                lugares.add(new Lugar(nombre,latitud,longitud,iconoId,categoria));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return lugares;
+    }
+    private String loadJSONFromAsset() {
+        String json;
+        try {
+            InputStream is = getAssets().open("lugares.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+    private int getIconoId(String iconoNombre){
+        switch (iconoNombre){
+            case "ic_park":
+                return R.drawable.ic_park;
+            case "ic_museum":
+                return R.drawable.ic_museum;
+            case "ic_mall":
+                return R.drawable.ic_mall;
+            case "ic_stadium":
+                return R.drawable.ic_stadium;
+            case "ic_atraccions":
+                return R.drawable.ic_atraccions;
+            case "ic_tourism":
+                return R.drawable.ic_tourism;
+            default:
+                // Un ícono por defecto si no se encuentra
+                return R.drawable.ic_park;
+        }
     }
     @Override
     protected void onResume() {
